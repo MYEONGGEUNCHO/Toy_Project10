7f625531dd8ea889310f6e9eb336c6d17a6c0237
mgcho <mgcho@egap.co.kr>
2022-12-09 10:14:32 +0900

    chapter2

---
+++ src/app/test_orm.py
@@ -0,0 +1,17 @@
+import model
+from datetime import date
+
+# ORM 직접 테스트
+def test_orderline_mapper_can_load_lines(session):
+    session.execute(
+        "INSERT INTO order_lines (orderid, sku, qty) VALUES "
+        '("order1", "RED-CHAIR", 12),'
+        '("order1", "RED-TABLE", 13),'
+        '("order2", "BLUE-LIPSTICK", 14)'
+    )
+    expected = [
+        model.OrderLine("order1", "RED-CHAIR", 12),
+        model.OrderLine("order1", "RED-TABLE", 13),
+        model.OrderLine("order2", "BLUE-LIPSTICK", 14),
+    ]
+    assert session.query(model.OrderLine).all() == expected
